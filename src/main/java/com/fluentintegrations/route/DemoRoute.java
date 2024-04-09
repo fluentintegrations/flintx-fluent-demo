49be231addc4d5253548dbc6b275415aef865051
fluentintegrations <github@fluentintegrations.com>
2024-04-09 11:15:48 +1000

    Refactorings to split out product processing

--- src/main/java/com/fluentintegrations/route/DemoRoute.java
+++ src/main/java/com/fluentintegrations/route/DemoRoute.java
@@ -7,10 +7,12 @@ import org.apache.camel.model.dataformat.JsonLibrary;
 
 @ApplicationScoped
 public class DemoRoute extends RouteBuilder {
+    public static final String ROUTE_URI_PROCESS_PRODUCT = "direct:process-product";
     @Override
     public void configure() {
         declareGoogleRoute();
         declareKafkaRoute();
+        declareProcessProductRoute();
     }
     private void declareGoogleRoute() {
         // poll google cloud storage bucket for new files
@@ -19,36 +21,38 @@ public class DemoRoute extends RouteBuilder {
                 .log("Processing new file from Google Cloud Storage ${header.CamelGoogleCloudStorageObjectName}")
                 // split files into individual product records
                 .split().jsonpath("$.products")
-                .log("Processing product record: ${body}")
-                // map each product record using the mapstruct
-                .to("bean:com.fluentintegrations.mapping.DemoProductMappingBean")
-                .log("FlintX product: ${body}")
-                // send each record to the flintx fluent product service
-                .marshal().json(JsonLibrary.Jackson)
-                .setHeader("Content-Type").simple("application/json")
-                .setHeader("apiKey").simple("{{flintx.apikey}}")
-                .toD("{{flintx-fluent-product-service.url}}/fluent/product/load")
-                .log("Response: ${body}")
+                    .to(ROUTE_URI_PROCESS_PRODUCT)
                 .end()
         ;
     }
     private void declareKafkaRoute() {
         from("{{kafka.event.uri}}")
                 .routeId("demo-kafka-route")
-                .unmarshal().json(JsonLibrary.Jackson)
+                .log("Processing product update from kafka topic {{kafka.event.uri}}")
+                // split message into individual product records
                 .split().jsonpath("$.products").streaming()
+                    .to(ROUTE_URI_PROCESS_PRODUCT)
+                    .to("{{kafka.eventVerification.uri}}")
+                .end()
+        ;
+    }
+
+    private void declareProcessProductRoute() {
+        from(ROUTE_URI_PROCESS_PRODUCT)
                 .log("Mapping product:${body}")
+                // map each product record using the mapstruct
                 .to("bean:com.fluentintegrations.mapping.DemoProductMappingBean")
                 .log("Mapped product:${body}")
                 .marshal().json(JsonLibrary.Jackson)
+                // send each record to the flintx fluent product service
                 .log("Sending product: ${body}")
                 .setHeader(Exchange.HTTP_METHOD).simple("POST")
                 .setHeader("Content-Type").simple("application/json")
                 .setHeader("apiKey").simple("{{flintx.apikey}}")
                 .toD("{{flintx-fluent-product-service.url}}/fluent/product/load?bridgeEndpoint=true")
                 .log("Response: ${body}")
-                .to("{{kafka.eventVerification.uri}}")
-                .end()
         ;
+
     }
 }
+
